8c784c0b521103b8f6edb2b5e22c09cd579f19ed
J.H.PARK <jihyung.park@my.jcu.edu.au>
2022-08-27 19:35:30 +1000

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World from the IntelliJ IDE!");
+
+        int studentAge = 15;
+        double studentGPA = 3.45;
+        char studentFirstInitial = 'k';
+        char studentLastInitial = 'H';
+        boolean hasPerfectAttendance = true;
+        String studentFirstName = "Joey";
+        String studentLastName = "Park";
+
+        System.out.println(studentAge);
+        System.out.println(studentGPA);
+        System.out.println(studentFirstInitial);
+        System.out.println(studentLastInitial);
+        System.out.println(hasPerfectAttendance);
+        System.out.println(studentFirstName);
+        System.out.println(studentLastName);
+
+ 
+    }
+}
